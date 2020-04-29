a79a36c1a55b113e914c665cfb0ac0f0f9b7778f
Josh Mu <mu@joshmu.com>
2020-04-29 21:39:58 +1000

    custom twitter api integrated

--- js/twitterSetup.js
+++ js/twitterSetup.js
@@ -3,65 +3,43 @@ $(document).ready(function () {
    * Twitter
   /* ---------------------------------------------- */
 
-  /**
-   * ### HOW TO CREATE A VALID ID TO USE: ###
-   * Go to www.twitter.com and sign in as normal, go to your settings page.
-   * Go to "Widgets" on the left hand side.
-   * Create a new widget for what you need eg "user time line" or "search" etc.
-   * Feel free to check "exclude replies" if you don't want replies in results.
-   * Now go back to settings page, and then go back to widgets page and
-   * you should see the widget you just created. Click edit.
-   * Look at the URL in your web browser, you will see a long number like this:
-   * 345735908357048478
-   * Use this as your ID below instead!
-   */
-
-  // ! TWITTER ISN'T WORKING, NEED TO MOVE TO USING OAUTH1, CREDS ARE IN MY NOTES ON MY LOCAL COMP
-  // so... removing any mention of twitter for now
-  $('.twitter-logo').remove()
-
-  // new verion!!! widget id is deprecated
-  // var config = {
-  //   profile: { screenName: 'josh_mu_' },
-  //   domId: 'twitterNews',
-  //   maxTweets: 5,
-  //   enableLinks: true,
-  //   showUser: false,
-  //   showTime: true,
-  //   showImages: false,
-  //   showInteraction: false,
-  //   customCallback: handleTweets,
-  //   lang: 'en',
-  // }
-
   // USING MY CUSTOM API
   fetch('https://mu-twitter-timeline-api.herokuapp.com/')
     .then((response) => response.json())
     .then((data) => handleTweets(data))
 
+  function tweetHtml(tweet) {
+    return `<div>
+              <p>${tweet.full_text}</p>
+            </div>`
+  }
+
   function handleTweets(tweets) {
     console.log('tweet tweet')
     console.log(tweets)
+    // let's just use 10 tweets
+    var tweets = tweets.slice(0, 10)
     var x = tweets.length
     var n = 0
     var html = ''
     while (n < x) {
-      html += '<div>' + tweets[n] + '</div>'
+      html += tweetHtml(tweets[n])
       n++
     }
     var $twitterNews = $('#twitterNews')
 
     // apply tweet data
     $twitterNews.html(html)
-    // remove emojis
-    $twitterNews.find('img').remove()
-    // remove 'Posted on' text
-    $twitterNews.find('.timePosted').each(function (index, element) {
-      var $elem = $(element)
-      var text = $elem.children('a').text()
-      var text = text.replace('Posted on ', '')
-      $elem.children('a').text(text)
-    })
+
+    // // remove emojis
+    // $twitterNews.find('img').remove()
+    // // remove 'Posted on' text
+    // $twitterNews.find('.timePosted').each(function (index, element) {
+    //   var $elem = $(element)
+    //   var text = $elem.children('a').text()
+    //   var text = text.replace('Posted on ', '')
+    //   $elem.children('a').text(text)
+    // })
 
     // init carousel
     $twitterNews.owlCarousel({
@@ -79,5 +57,5 @@ $(document).ready(function () {
     })
   }
 
-  twitterFetcher.fetch(config)
+  // twitterFetcher.fetch(config)
 })
